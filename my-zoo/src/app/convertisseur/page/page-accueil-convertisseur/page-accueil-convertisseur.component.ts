5a9b94ca9b6b046f5fe532f7d62068fb111a8cf1
Sandrine <sandrine.godmez@gmail.com>
2019-04-30 04:31:32 +0200

    Convertisseur OK

--- my-zoo/src/app/convertisseur/page/page-accueil-convertisseur/page-accueil-convertisseur.component.ts
+++ my-zoo/src/app/convertisseur/page/page-accueil-convertisseur/page-accueil-convertisseur.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-accueil-convertisseur',
-  templateUrl: './page-accueil-convertisseur.component.html',
-  styleUrls: ['./page-accueil-convertisseur.component.css']
+    selector: 'zoo-page-accueil-convertisseur',
+    templateUrl: './page-accueil-convertisseur.component.html',
+    styleUrls: ['./page-accueil-convertisseur.component.css']
 })
 export class PageAccueilConvertisseurComponent implements OnInit {
 
-  constructor() { }
+    public miles: number;
+    public metres: number;
+    private COEF: number = 1609;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.metres = 0;
+        this.miles = 0;
+    }
+
+    public onKeyMiles(miles: string) {
+        console.log(miles);
+        this.miles = Number.parseInt(miles);
+        this.metres = this.miles * this.COEF;
+    }
+
+    public onKeyMetres(metres: string) {
+        this.metres = Number.parseInt(metres);
+        this.miles = this.metres / this.COEF;
+    }
 
 }
